ef7d9ab5ab98f0e052dff8e6fdd8e12fa6bcc3bf
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-02 07:34:25 +0200

    feat : updat varabils key

--- src/notifications/firebase.ts
+++ src/notifications/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getMessaging, getToken } from "firebase/messaging"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBW2KxDuLgUyek3LTqTJ29s9-hU4D2Y8lA",
-    authDomain: "pushnotifications-565d2.firebaseapp.com",
-    projectId: "pushnotifications-565d2",
-    storageBucket: "pushnotifications-565d2.firebasestorage.app",
-    messagingSenderId: "779885201511",
-    appId: "1:779885201511",
-    measurementId: "G-CPCBYBE4KY",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
