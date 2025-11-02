9b33842d5d5e619aef1678e4cc20f8db565acd9e
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-02 23:18:17 +0200

    fix : fix varilabels

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,17 +1,40 @@
-import { onMessage } from "firebase/messaging"
-import { toast } from "sonner"
+import { onMessage } from "firebase/messaging";
+import { toast } from "sonner";
 
-import { lazy, useEffect } from "react"
-import { Outlet } from "react-router"
 
-import { SidebarProvider } from "@/components/ui/sidebar"
 
-import DashboardSidebar from "@/components/dashboard-sidebar"
-import { generateToken, messaging } from "@/notifications/firebase"
+import { lazy, useEffect } from "react";
+import { Outlet } from "react-router";
+
+
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+
+
+import DashboardSidebar from "@/components/dashboard-sidebar";
+import { generateToken, messaging } from "@/notifications/firebase";
+
+
+
+
 
 const Header = lazy(() => import("@/components/header"))
 
 export default function DashboardLayout() {
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker
+                .register("/firebase-messaging-sw.js")
+                .then((registration) => {
+                    console.log("✅ Service Worker registered:", registration)
+                })
+                .catch((err) => {
+                    console.error("❌ Service Worker registration failed:", err)
+                })
+        }
+    }, [])
+
     useEffect(() => {
         generateToken()
         onMessage(messaging, (payload) => {
