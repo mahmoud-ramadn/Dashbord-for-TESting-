0a563262ea466e948357c8cc4fa312ec7a3bcdc9
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-02 08:11:56 +0200

    feat : updat varabils key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js")
-importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js")
 
 // تهيئة تطبيق Firebase
 firebase.initializeApp({
