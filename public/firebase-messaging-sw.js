0f7897e7e980b2eff3a4755fb852396bf145c514
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-02 07:27:26 +0200

    feat : updat varabils key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-com
 
 // تهيئة تطبيق Firebase
 firebase.initializeApp({
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyBW2KxDuLgUyek3LTqTJ29s9-hU4D2Y8lA",
+    authDomain: "pushnotifications-565d2.firebaseapp.com",
+    projectId: "pushnotifications-565d2",
+    storageBucket: "pushnotifications-565d2.firebasestorage.app",
+    messagingSenderId: "779885201511",
+    appId: "1:779885201511",
+    measurementId: "G-CPCBYBE4KY",
 })
 
 // تفعيل خدمة المراسلة
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,6 +42,7 @@ export default function Index() {
             toast.success(`اشعار جديد: ${payload.notification?.title} - ${payload.notification?.body}`)
         })
     }, [])
+    
     return (
         <div className="min-h-screen p-4 sm:p-6 lg:p-8">
             <div className="max-w-7xl mx-auto space-y-8">
